ef2ac6c5c63dcaf81a8e9b84054b7b9bb10af4dd
João Scheleder Neto <scheleder@yahoo.com>
2022-10-25 08:24:53 -0300

    Add files via upload

---
+++ SlidePictures/src/example/Imagem.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+/**
+ *
+ * @author jscheleder
+ */
+public class Imagem {
+    public String nome;
+
+    public Imagem() {
+    }
+
+    public Imagem(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+}
